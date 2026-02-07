f8c96799f858893056d814a34cd94ff58f58974e
TurkogluAta <x21247129@student.ncirl.ie>
2026-02-07 22:22:23 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Week 2 Lab – Bash & Git
+Ata Turkoglu - 21247129
+[devops-lab-2-task](https://github.com/TurkogluAta/devops-lab-2-task)
+
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -12,4 +12,12 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 # Create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
+# Set permissions for files ending with _1.txt and _3.txt (read/write for owner only)
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt fol_2/2_3.txt
+
+# Set permissions for files ending with _2.txt (read/write/execute for all users)
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
+
 echo "Job completed"
